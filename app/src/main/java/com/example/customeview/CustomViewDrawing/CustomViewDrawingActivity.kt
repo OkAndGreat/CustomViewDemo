c8abad2bc6b778f1853e73586a505583ebfac0ac
wzt <2238091763@qq.com>
2021-04-10 21:32:52 +0800

    [update]饼状图View和仪表盘View

---
+++ app/src/main/java/com/example/customeview/CustomViewDrawing/CustomViewDrawingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.customeview.CustomViewDrawing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.customeview.R
+
+class CustomViewDrawingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_view_drawing)
+    }
+}
---
+++ app/src/main/java/com/example/customeview/util.kt
@@ -0,0 +1,13 @@
+package com.example.customeview
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+//这里也可以使用扩展函数
+//dp2px
+val Float.px
+    get() = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this,
+            Resources.getSystem().displayMetrics
+    )
