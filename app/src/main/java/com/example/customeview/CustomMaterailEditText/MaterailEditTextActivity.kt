506d6ead8015c5850fdde32448d5bd75df6668c0
wzt <2238091763@qq.com>
2021-04-14 18:39:29 +0800

    [upadte]MaterailEditText

---
+++ app/src/main/java/com/example/customeview/CustomMaterailEditText/MaterailEditTextActivity.kt
@@ -0,0 +1,12 @@
+package com.example.customeview.CustomMaterailEditText
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.customeview.R
+
+class MaterailEditTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materail_edit_text)
+    }
+}
---
+++ app/src/main/java/com/example/customeview/CustomMaterailEditText/MaterialEditText.kt
@@ -0,0 +1,56 @@
+package com.example.customeview.CustomMaterailEditText
+
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.text.BoringLayout
+import android.text.Layout
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
+import com.example.customeview.dp
+
+private val TEXT_SIZE = 12.dp
+private val HORIZONTAL_OFFSET = 5.dp
+private val VERTICAL_OFFSET = 23.dp
+private val EXTRA_VERTICAL_OFFSET = 16.dp
+private const val TAG = "MaterialEditText"
+
+class MaterialEditText(context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var floatingLabelShown = false
+    private var floatingLabelFraction = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+    private val animator by lazy {
+        ObjectAnimator.ofFloat(this, "floatingLabelFraction", 0f, 1f)
+    }
+
+    init {
+        paint.textSize = TEXT_SIZE
+    }
+
+
+    override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
+        if (floatingLabelShown && text.isNullOrEmpty()) {
+            floatingLabelShown = false
+            animator.reverse()
+        } else if (!floatingLabelShown && !text.isNullOrEmpty()) {
+            floatingLabelShown = true
+            animator.start()
+        }
+        Log.d(TAG, "onTextChanged:$lengthAfter ")
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        paint.alpha = (floatingLabelFraction * 0xff).toInt()
+        val currentVertivalValue = VERTICAL_OFFSET + EXTRA_VERTICAL_OFFSET * (1 - floatingLabelFraction)
+        canvas.drawText(hint.toString(), HORIZONTAL_OFFSET, currentVertivalValue, paint)
+    }
+}
