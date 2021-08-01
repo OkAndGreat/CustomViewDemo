29c58d3ac2ea5df32d10b75709bf3c05ad8ed364
wzt <2238091763@qq.com>
2021-08-01 23:15:48 +0800

    进行kotlin版本移植

--- app/src/main/java/com/example/customview/CustomProgressBar/CountView.kt
+++ app/src/main/java/com/example/customview/CustomProgressBar/CountView.kt
@@ -1,10 +1,200 @@
 package com.example.customview.CustomProgressBar
 
+import android.animation.ObjectAnimator
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.view.View
+import com.example.customview.TextChangeView.Other.changed.TuvPoint
+import com.example.customview.TextChangeView.Other.changed.TuvUtils
+import kotlin.math.abs
+import kotlin.math.ceil
 
 class CountView(context: Context) : View(context) {
+    companion object{
+        const val DEFAULT_TEXT_COLOR = "#cccccc"
+        const val DEFAULT_TEXT_SIZE = 45f
+        const val COUNT_ANIM_DURING = 5000
+    }
+
+    private var mTextPaint:Paint
+    private var mTextSize = DEFAULT_TEXT_SIZE
+    private var mTextColor = 0
+    private var mEndTextColor = 0
+
+    private var mCount = 0
+    private var mMaxCount = 0
+    //mTexts[0]是不变的部分，mTexts[1]原来的部分，mTexts[2]变化后的部分
+    private var mTexts: Array<String?> = arrayOfNulls(3)
+
+    //表示各部分的坐标
+    private var mTextPoints
+            : Array<TuvPoint> = arrayOf(TuvPoint(), TuvPoint(),TuvPoint(),TuvPoint())
+
+    private var mMaxOffsetY = 0f
+    private var mMinOffsetY = 0f
+
+    private var mOldOffsetY = 0f
+    private var mNewOffsetY = 0f
+    private var mFraction = 0f
+
+    private var mCarryNum = 0
+
+    private var mCount2Bigger = false
+
+    init {
+        calculateChangeNum(0)
+
+        mTextColor = Color.parseColor(DEFAULT_TEXT_COLOR)
+
+        mMinOffsetY = 0f
+        mMaxOffsetY = mTextSize
+
+        mEndTextColor =
+            Color.argb(0, Color.red(mTextColor), Color.green(mTextColor), Color.blue(mTextColor))
+
+        mTextPaint = Paint()
+        mTextPaint.isAntiAlias = true
+        mTextPaint.textSize = mTextSize
+        mTextPaint.color = mTextColor
+    }
+
+    fun setCount(mCount: Int) {
+        this.mCount = mCount
+        calculateChangeNum(0)
+        requestLayout()
+    }
+
+    fun setMaxCount(mMaxCount: Int) {
+        this.mMaxCount = mMaxCount
+    }
 
 
+    fun setTextSize(mTextSize: Float) {
+        this.mTextSize = mTextSize
+        requestLayout()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(
+            TuvUtils.getDefaultSize(
+                widthMeasureSpec,
+                getContentWidth() + paddingLeft + paddingRight * 2
+            ),
+            TuvUtils.getDefaultSize(
+                heightMeasureSpec,
+                getContentHeight() + paddingTop + paddingBottom
+            )
+        )
+    }
+
+    private fun getContentWidth(): Int {
+        return ceil(mTextPaint.measureText("$mCount   /  $mMaxCount"))
+            .toInt()
+    }
+
+    private fun getContentHeight(): Int {
+        return mTextSize.toInt()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        calculateLocation()
+    }
+
+    private fun calculateLocation() {
+        val text = mCount.toString()
+        val textWidth = mTextPaint.measureText(text) / text.length
+        val unChangeWidth = textWidth * (mTexts[0]?.length ?: 0)
+        val fontMetrics = mTextPaint.fontMetricsInt
+        val y =
+            (paddingTop + (getContentHeight() - fontMetrics.bottom - fontMetrics.top) / 2).toFloat()
+        mTextPoints[0].x = paddingLeft.toFloat()
+        mTextPoints[1].x = paddingLeft + unChangeWidth
+        mTextPoints[2].x = paddingLeft + unChangeWidth
+        mTextPoints[3].x = paddingLeft + unChangeWidth + (1 - mFraction) * (textWidth * mCarryNum)
+        mTextPoints[0].y = y
+        mTextPoints[1].y = y - mOldOffsetY
+        mTextPoints[2].y = y - mNewOffsetY
+        mTextPoints[3].y = y
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        //不变的部分
+        mTextPaint.color = mTextColor
+        canvas.drawText(mTexts[0].toString(), mTextPoints[0].x, mTextPoints[0].y, mTextPaint)
+
+        //变化前部分
+        mTextPaint.color = (TuvUtils.evaluate(mFraction, mEndTextColor, mTextColor) as Int)
+        canvas.drawText(mTexts[1].toString(), mTextPoints[1].x, mTextPoints[1].y, mTextPaint)
+
+        //变化后部分
+        mTextPaint.color = (TuvUtils.evaluate(mFraction, mTextColor, mEndTextColor) as Int)
+        canvas.drawText(mTexts[2].toString(), mTextPoints[2].x, mTextPoints[2].y, mTextPaint)
+
+        //后缀
+        mTextPaint.color = mTextColor
+        canvas.drawText("   / $mMaxCount", mTextPoints[3].x, mTextPoints[3].y, mTextPaint)
+    }
+
+    /**
+     * 计算不变，原来，和改变后各部分的数字
+     * 这里是只针对加一和减一去计算的算法，因为直接设置的时候没有动画
+     */
+    fun calculateChangeNum(change: Int) {
+        if (change == 0) {
+            mTexts[0] = mCount.toString()
+            mTexts[1] = ""
+            mTexts[2] = ""
+            return
+        }
+
+        val oldNum = mCount.toString()
+        val newNum = (mCount + change).toString()
+
+        mCarryNum = newNum.length - oldNum.length
+
+        for (i in oldNum.indices) {
+            val oldC = oldNum[i]
+            val newC = newNum[i]
+            if (oldC != newC) {
+                mTexts[0] = if (i == 0) "" else newNum.substring(0, i)
+                mTexts[1] = oldNum.substring(i)
+                mTexts[2] = newNum.substring(i)
+                break
+            }
+        }
+        mCount += change
+        startAnim(change > 0)
+    }
+
+    private fun startAnim(is2Bigger: Boolean) {
+        mCount2Bigger = is2Bigger
+        val textOffsetY = ObjectAnimator.ofFloat(
+            this, "textOffsetY",
+            mMinOffsetY, if (mCount2Bigger) mMaxOffsetY else -mMaxOffsetY
+        )
+        textOffsetY.duration = COUNT_ANIM_DURING.toLong()
+        textOffsetY.start()
+    }
+
+    fun setTextOffsetY(offsetY: Float) {
+        mOldOffsetY = offsetY //变大是从[0,1]，变小是[0,-1]
+        mNewOffsetY = if (mCount2Bigger)
+        {
+            //从下到上[-1,0]
+            offsetY - mMaxOffsetY
+        } else
+        {
+            //从上到下[1,0]
+            mMaxOffsetY + offsetY
+        }
+        mFraction = (mMaxOffsetY - abs(mOldOffsetY)) / (mMaxOffsetY - mMinOffsetY)
+        calculateLocation()
+        postInvalidate()
+    }
 
 }
--- app/src/main/java/com/example/customview/CustomProgressBar/ProgressLayout.kt
+++ app/src/main/java/com/example/customview/CustomProgressBar/ProgressLayout.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.widget.LinearLayout
 import com.example.customview.R
-import com.example.customview.TextChangeView.Other.changed.CountView
 
 class ProgressLayout(context: Context, attrs: AttributeSet?) :
     LinearLayout(context, attrs) {
@@ -21,7 +20,7 @@ class ProgressLayout(context: Context, attrs: AttributeSet?) :
         val typedArray =
             context.obtainStyledAttributes(attrs, R.styleable.ProgressLayout)
         maxCount = typedArray.getInt(R.styleable.ProgressLayout_max_count, 0)
-        curCount = typedArray.getInt(R.styleable.ProgressLayout_cur_count,0)
+        curCount = typedArray.getInt(R.styleable.ProgressLayout_cur_count, 0)
         mProgressView.setMaxCount(maxCount)
         mCountView.setMaxCount(maxCount)
         setCurCount(curCount)
@@ -33,16 +32,18 @@ class ProgressLayout(context: Context, attrs: AttributeSet?) :
         orientation = VERTICAL
 
         addCountView()
-        mCountView.count = 0
+        mCountView.setCount(0)
         addProgressView()
     }
 
+    //将ProgressView添加到线性布局中
     private fun addProgressView() {
         mProgressView = ProgressView(context)
         addView(mProgressView, getProgressViewParams())
     }
 
     //params.xxxMargin = paddingxxx 是为了处理 ProgressLayout设置padding时它内部控件的摆放情况
+    //生成ProgressView的布局参数
     private fun getProgressViewParams(): LayoutParams {
         val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
         params.topMargin = paddingTop
@@ -51,22 +52,26 @@ class ProgressLayout(context: Context, attrs: AttributeSet?) :
         return params
     }
 
+    //将CountView添加到线性布局中
     private fun addCountView() {
         mCountView = CountView(context)
         addView(mCountView, getCountViewParams())
     }
 
+    //生成CountView的布局参数
     private fun getCountViewParams(): LayoutParams {
         val params = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
         params.leftMargin = paddingLeft
         params.rightMargin = paddingRight
-        //这里加20的目的是为了让TextView和进度条View有一定间隔
+        //这里加20的目的是为了让CountView和进度条View有一定间隔
         params.bottomMargin = paddingBottom + 20
         params.gravity = Gravity.CENTER
         return params
     }
 
-
+    //进入页面拿到后端数据后应该马上调这个方法来设置当前的数量
+    //如果没有设置，则默认为0
+    //即  0  /  maxCount
     fun setCurCount(Count: Int) {
         mProgressView.setCurCount(Count)
         mCountView.calculateChangeNum(Count)
